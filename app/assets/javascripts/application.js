33b8dd1fe7346377f86c65ee0ad16e7cf874435b
Sam Vary <sam.vary@gmail.com>
2016-02-13 14:06:07 -0500

    added js sprockets and actual files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+//= require jquery.min.js
+//= require moderniz.min.js
+//= require jquery-migrate.min.js
+//= require bootstrap.min.js
+//= require jquery.flexslider-min.js
+//= require wow.min.js
+//= require jquery.stick.js
+//= require bootstrap-hover-dropdown.min.js
+//= require jquery.stellar.min.js
+//= require owl.carousel.min.js
+//= require jquery.mb.YTPlayer.min.js
+//= require waypoints.min.js
+//= require easypiechart.js
+//= require jquery.isotope.min.js
+//= require jquery.imagesloaded.min.js
+//= require jquery.counterup.min.js
+//= require jquery.countdown.js
+//= require contact_me.js
+//= require jquery.themepunch.tools.min.js
+//= require jquery.themepunch.revolution.min.js
+//= require revolution-custom.js
+//= require isotope-custom.js
+//= require pace.min.js
+
